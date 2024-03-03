9b74a98e65fa218af70306bff699d24912baeac3
yxing6 <yxing6@student.ubc.ca>
2024-03-03 13:42:44 -0800

    call to the register() function in ~/enph353_gym-gazebo-noetic/gym_gazebo/__init__.py to register the environment with gym-gazebo

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -4,7 +4,11 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 # Gazebo
-# ----------------------------------------
+register(
+    id='Gazebo_linefollow-v0',
+    entry_point='gym_gazebo.envs.gazebo_linefollow:Gazebo_Linefollow_Env',
+    max_episode_steps=3000,
+)
 
 # cart pole
 register(
